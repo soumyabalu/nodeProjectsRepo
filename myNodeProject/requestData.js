25748a1237ef3f87887a300c20cbd228b2fe0835
Soumya Lakshmi <soumyalakshmi@Soumyas-MacBook-Air.local>
2024-01-30 12:33:56 -0500

    adding new file

---
+++ myNodeProject/requestData.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var filename = "." + q.pathname;
+    fs.readFile('filename', function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write('404 not found');
+        }
+        console.log("Sucess");
+    })
+    res.write(data);
+    return res.end();
+    fs.readFile('filename', function(err,data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write('404 not found');
+        }
+        console.log("Sucess");
+    })
+    res.write(data);
+    return res.end();
+
+})
