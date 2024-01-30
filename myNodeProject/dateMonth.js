fd38ff7c7f4459088086b1d7734596fde1358fbb
Soumya <55769157+soumyabalu@users.noreply.github.com>
2024-01-30 10:20:11 -0500

    Update dateMonth.js

--- myNodeProject/dateMonth.js
+++ myNodeProject/dateMonth.js
@@ -1,10 +1,10 @@
-var http = require('http');    // importing the inbuild http module to node.js
-var url = require('url');  // importing the inbuild url modeule to node.js to split the query string into ore redable format
+var http = require('http');   
+var url = require('url'); 
 
 http.createServer(function(req, res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    var q = url.parse(req.url, true);
-    var text = ("q.Date" + " " + "q.Month");
-    res.end(text);
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
 
 }).listen(8080);
