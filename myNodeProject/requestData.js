c3d819f44ac87439eba741d216a402ed483d2472
Soumya <55769157+soumyabalu@users.noreply.github.com>
2024-01-30 12:59:46 -0500

    Update requestData.js

--- myNodeProject/requestData.js
+++ myNodeProject/requestData.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 http.createServer(function(req, res){
-    var q = url.parse(req.url, true).query;
+    var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
     fs.readFile(filename, function(err, data){
         if (err) {
