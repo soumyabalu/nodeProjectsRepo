026bf3fb0a5a840f43b84401d6a799989c03ac8e
Soumya <55769157+soumyabalu@users.noreply.github.com>
2024-01-30 12:54:03 -0500

    Update requestData.js

--- myNodeProject/requestData.js
+++ myNodeProject/requestData.js
@@ -5,23 +5,17 @@ http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
     var filename = "." + q.pathname;
-    fs.readFile('filename', function(err, data){
+    fs.readFile(filename, function(err, data){
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.write('404 not found');
         }
-        console.log("Sucess");
+       
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
     })
-    res.write(data);
-    return res.end();
-    fs.readFile('filename', function(err,data){
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write('404 not found');
-        }
-        console.log("Sucess");
-    })
-    res.write(data);
-    return res.end();
+    
+    
 
 }).listen(8080);
