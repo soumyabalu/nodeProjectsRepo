4507c9e83e497dd0815d7cd44031be8b23a26beb
Soumya Lakshmi <soumyalakshmi@Soumyas-MacBook-Air.local>
2024-01-31 11:25:26 -0500

    adding files

--- myNodeProject/sampleRead1.js
+++ myNodeProject/sampleRead1.js
@@ -1,15 +1,21 @@
 var http = require("http");
+var url = require("url");
 var fs = require("fs");
 http.createServer(function(req, res){
-    var datareq = "." + "/pathname";
-    fs.readFile(datareq, function(err, data){
+    
+var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data){
         if (err){
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            return res.end(data);
+            res.writeHead(404, {'Content-Type': 'text/txt'});
+            return res.write("404 not found");
         }
-        {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            return res.end(data);
-        };
+        
+            res.writeHead(200, {'Content-Type': 'text/txt'});
+            res.write(data);
+            return res.end();
+            
+       
     });
 }).listen(8080);
+
